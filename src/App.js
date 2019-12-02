77e3be10548650e12f9e710720d45cbda3c1d34e
Patrice Whitten <patricewhitten@Patrices-MBP-2.lan1>
2019-12-01 23:38:10 -0600

    cartFunctionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import React from 'react';
+
 import Landing from './components/Landing';
 import Nav from './components/Nav';
 //import Footer from './components/Footer';
 import Header from './components/Header';
 import OurShades from './components/OurShades';
+import React, { Component } from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import Cart from './components/Cart'
 
 
 
- 
-function App () {
-  return (
-    <div>
-      <Nav />
-      <Header />
-      <Landing />
-      <OurShades />
+class App extends Component {
+  render() {
+    return (
+       <BrowserRouter>
+            <div className="App">
+            
+              <Nav/>
+              
+              <OurShades />
+                <Switch>
+                    <Route exact path="/" component={Landing}/>
+                    <Route path="/cart" component={Cart}/>
+                  </Switch>
+             </div>
+       </BrowserRouter>
       
-    </div>
-  );
+    );
+  }
 }
-
 
 export default App;
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,45 +1,39 @@
 import React from "react";
+
 import "./Nav.css";
 
-function Nav() {
+const Nav = () => {
   return (
-    <>
-
-
-
-      <p class="navbar-header"> Sunglass Chic </p>
-
-      <ul class="nav nav-tabs">
-        <li class="nav-item">
-          <a class="nav-link" href="/#">
-            {" "}
-            Home{" "}
-          </a>
-        </li>
-
-        <li class="nav-item">
-          <a class="nav-link" href="/#">
-            {" "}
-            Our Shades{" "}
-          </a>
-        </li>
-
-        <li class="nav-item">
-          <a class="nav-link" href="/#">
-            {" "}
-            Meet the Squad{" "}
-          </a>
-        </li>
-      </ul>
-
-
-
-
-
-
+    <nav>
+      <div>
       
-    </>
+        <p class="navbar-header"> Sunglass Chic </p>
+
+        <ul class="nav nav-tabs">
+          <li class="nav-item">
+            <a class="nav-link" href="/#">
+              {" "}
+              Home{" "}
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="/#">
+              {" "}
+              Our Shades{" "}
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="/#">
+              {" "}
+              Meet the Squad{" "}
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
-}
+};
 
 export default Nav;
